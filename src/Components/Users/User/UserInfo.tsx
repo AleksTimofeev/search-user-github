ac82381a3e4c89cd6580adfe872f95d9975c0e44
AleksTimofeev <timoha144a@gmail.com>
2023-07-31 16:35:44 +0300

    add userInfo component

---
+++ src/Components/Users/User/UserInfo.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import styles from './User.module.scss'
+import {useAppDispatch, useAppSelector} from "../../../store/store";
+import {getUserInfo} from "../usersReducer";
+
+type PropsType = {
+  closeInfo: () => void
+  url: string
+}
+
+export const UserInfo: React.FC<PropsType> = ({closeInfo, url}) => {
+
+  const userInfo = useAppSelector(state => state.users.userInfo)
+  const userInfoStatus = useAppSelector(state => state.users.userInfoStatus)
+  const dispatch = useAppDispatch()
+
+  const handleClose = () => {
+    closeInfo()
+  }
+
+  useEffect(() => {
+    dispatch(getUserInfo({url}))
+  }, [])
+
+  return (
+    <div className={styles.userInfo}>
+      {userInfoStatus === 'idle' && userInfo && <>
+        <button onClick={handleClose}>X</button>
+        <div className={styles.header}>
+          <img src={userInfo.avatar_url} width='100' alt="img"/>
+          <div className={styles.title}>
+            <h3>{userInfo.login}</h3>
+            <a href={userInfo.html_url}>github</a>
+          </div>
+        </div>
+        <span>location - {userInfo.location}</span>
+        <span>email - {userInfo.email}</span>
+        <span>company - {userInfo.company}</span>
+        <span>followers - {userInfo.followers}</span>
+        <span>public repos - {userInfo.public_repos}</span>
+      </>}
+    </div>
+  );
+}
